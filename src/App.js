1e38df99b31bb04a36709e2b4a44b0298022c349
Afsha10 <unityofbeing1@gmail.com>
2023-06-16 22:54:09 +0100

    Completed Exercise G
    Completed Exercise G from https://syllabus.codeyourfuture.io/react/week-1/lesson

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 const Logo = () => {
+  const appName = "Afsha's Pokedox";
   return (
     <div>
-      <h1>Welcome to the Pokedex</h1>
+      <h1>{appName}</h1>
       <img
         src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
         alt="of a bird"
@@ -13,14 +14,32 @@ const Logo = () => {
 };
 
 const BestPokemon = () => {
-  return <p>My favourite Pokemon is Squirtle</p>;
+  const abilities = ["Anticipation", "Adaptability", "Run-Away"];
+  return (
+    <div>
+      <p>My favourite Pokemon is Squirtle</p>
+      <ul>
+        {
+          abilities.map((ability) => {
+            return <li key={ability}>{ability}</li>;
+          })
+        }
+      </ul>
+    </div>
+  );
 };
+
+function CaughtPokemon() {
+  const date = new Date().toLocaleDateString();
+  return <p>Caught 0 Pokemon on {date}</p>;
+}
 
 function App() {
   return (
     <div>
       <Logo />
       <BestPokemon />
+      <CaughtPokemon />
     </div>
   );
 }

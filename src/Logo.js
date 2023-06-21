56f444249bb4010df732658bc368675f303c81d1
Afsha10 <unityofbeing1@gmail.com>
2023-06-21 19:06:52 +0100

    Completed Week2 Task A
    https://syllabus.codeyourfuture.io/react/week-2/lesson

--- src/Logo.js
+++ src/Logo.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 const Logo = (props) => {
+  function logWhenClicked() {
+    console.log("Hello My Puddings!");
+  }
   console.log(props);
   return (
     <div>
       <h1>{props.appName}</h1>
-      <img
+      <img onClick={logWhenClicked}
         src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
         alt="of a bird"
       />
